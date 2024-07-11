7001aca1f0ee2e0bc2e5fb66073774308e717578
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2024-07-11 18:30:43 -0300

    TESTANDO PYTHON

---
+++ Atividade_Extra_Python/ex3.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número: '))
+
+triplo = n * 3
+dobro = n * 2
+raiz = n ** (1/2)
+
+print(f'O dobro de {n} é {dobro}')
+print(f'O triplo de {n} é {triplo}')
+print(f'A raiz quadrada de {n} é {raiz :.2f}')
---
+++ Atividade_Extra_Python/ex5.py
@@ -0,0 +1,17 @@
+m = float(input('Digite uma distância em metros: '))
+
+# km hm dam me dem cm mm
+
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dec = m * 10
+cm = m * 100
+mm = m * 1000
+
+print(f'Km = {km}')
+print(f'Hm = {hm}')
+print(f'Dam = {dam}')
+print(f'dec = {dec}')
+print(f'cm = {cm}')
+print(f'mm = {mm}')
