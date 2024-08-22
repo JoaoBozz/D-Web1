dba14f21abfa6364b235c94c1a2edeb283b9b3a0
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2024-08-22 09:03:26 -0300

    atividadeatt

---
+++ Atividades Bimestre/22-08/script.js
@@ -0,0 +1,9 @@
+        //ENTRADA
+        let peso = 80
+        let altura = 1.8
+
+        //PROCESSAMENTO
+        let imc = peso / (altura * altura)
+
+        //SAÍDA
+        console.log("IMC: ", imc)
