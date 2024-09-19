87825d0aa5eba4895ea96bde370b0d70a1270864
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2024-09-19 10:02:08 -0300

    terminei lacos

---
+++ Atividades_Bimestre/19-09/script.js
@@ -0,0 +1,26 @@
+function tabuada(n) {
+    
+    let contador = 1
+    console.log("\n Tabuada do ", n)
+    
+    while(contador <= 10){
+        console.log(n, ' x ',contador, ' = ',n * contador)
+        contador += 1
+    }
+}
+
+tabuada(3)
+
+//OU 
+
+function tabuadas(y) {
+    
+    let contadores
+    console.log("\n Tabuada do ", y)
+    
+    for(contadores = 1; contadores <= 10; contadores += 1){
+        console.log(y, ' x ',contadores, ' = ',y * contadores)
+    }
+}
+
+tabuadas(5)
