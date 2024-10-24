b6d1cd16d06b486668c3096226913680e1bfaad8
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2024-10-24 10:07:17 -0300

    negocio de DW

---
+++ Atividade_Extra_Python/template/script.js
@@ -0,0 +1,18 @@
+const hamburgerIcon = document.querySelector("#hamburgerIcon")
+const closeIcon = document.querySelector("#closeIcon")
+
+// Associar funções aos eventos dos elementos HTML
+hamburgerIcon.addEventListener("click", showSidebar)
+closeIcon.addEventListener("click", hideSidebar)
+
+// Funções para abrir e fechar o sideBar
+function showSidebar(){
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.style.display = "flex";
+    console.log("teste")
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.style.display = "none";
+}
