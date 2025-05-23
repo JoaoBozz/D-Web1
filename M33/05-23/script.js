e38fd2e1a7fc75c53c780eee9bd8f9576e37260f
JoaoBozz <163414633+JoaoBozz@users.noreply.github.com>
2025-05-23 09:49:34 -0300

    buscarcep

---
+++ M33/05-23/script.js
@@ -0,0 +1,24 @@
+function buscarCep() { 
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.erro) {
+                alert('CEP não encontrado!');
+                return;
+            }
+
+            document.getElementById('logradouro').value = data.logradouro;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('cidade').value = data.localidade;
+            document.getElementById('estado').value = data.estado;
+        })
+        .catch((error) => {
+            console.log('Erro na requisição:', error);
+            alert('Erro ao buscar o CEP.');
+        });
+}
